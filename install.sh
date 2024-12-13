933001f7a8434fcbe39ee45c1518c40618be39fb
Ryohei Udagawa <r.udagawa1995@gmail.com>
2024-12-13 12:24:10 +0900

    install.shでbrew cleanupのコマンドを修正し、--forceオプションを追加

--- install.sh
+++ install.sh
@@ -1,12 +1,1 @@
-echo "run brew doctor ..."
-which brew >/dev/null 2>&1 && brew doctor
-
-echo "run brew update ..."
-which brew >/dev/null 2>&1 && brew update
-
-echo "ok. run brew upgrade ..."
-brew upgrade
-
-brew bundle
-
-brew cleanup
+brew bundle cleanup --force
